032d6a3a4fbaf05a9d140154459510260f1809bb
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-08-27 20:33:06 -0300

    Delete api directory

--- api/init.js
+++
@@ -1,9 +0,0 @@
-const db = require('./models');
-
-db.sequelize.sync({ force: true })
-  .then(() => {
-    console.log('Banco de dados sincronizado!');
-  })
-  .catch((error) => {
-    console.error('Erro ao sincronizar o banco de dados:', error);
-  });
